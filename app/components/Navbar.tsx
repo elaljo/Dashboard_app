46022801cd72741722f4cf7242786e323b9575fb
elaljo <elaljooo@gmail.com>
2025-11-25 17:53:09 +0100

    Creat Navbar component

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,32 @@
+import {
+    ClerkProvider,
+    SignInButton,
+    SignUpButton,
+    SignedIn,
+    SignedOut,
+    UserButton,
+  } from '@clerk/nextjs'
+
+import styles from "./Navbar.module.css"
+
+export default function Navbar(){
+    return(
+        <>
+            <div className={styles.AppName}> Dashboard </div>
+            <ul className={styles.ul}>
+                <li> Home </li>
+                <li> Agencies </li>
+                <li> Contacts </li>
+            </ul>
+            <div>
+                <SignedOut>
+                    <SignInButton />
+                </SignedOut>
+                <SignedIn>
+                    <UserButton />
+                </SignedIn>
+            </div>
+        </>
+
+    )
+}

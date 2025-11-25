e46855d683e1ad5818be1b4ca16f73b2cb35b6cd
elaljo <elaljooo@gmail.com>
2025-11-25 18:07:21 +0100

    moddified the className

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,10 @@
 import { SignIn } from '@clerk/nextjs'
+import styles from "./page.module.css"
 
 export default function Page() {
-  return <SignIn />
+    return(
+        <div className={styles.sign_in}>
+            <SignIn />
+        </div>
+    )
 }

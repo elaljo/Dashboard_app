dc9d304d9440f1b14d5daf64f4c38a6423ec812c
elaljo <elaljooo@gmail.com>
2025-11-25 19:40:07 +0100

    modified

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
 import styles from "./page.module.css";
-export default function Home() {
+import { currentUser } from "@clerk/nextjs/server";
+export default async function Home() {
+  const user = await currentUser()
+  const username = user?.username;
+  const welcomeSuffix = username ? `, ${username}` : '';
+
   return (
-    <div className={styles.page}>
+    <div>
+      <h1> Welcome{welcomeSuffix} 👋</h1>
+      <p> Welcome to our interactive dashboard!</p>
+      <p> Here, you can explore all our agencies and their associated employee contacts in one convenient place. Click on an agency to view its details and quickly access contact information for its employees. This tool makes it easy to navigate our network, stay connected, and find the information you need efficiently.</p>
     </div>
   );
 }

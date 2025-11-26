96b945a070a6e474278927dd39ee8abbfc1d8e24
elaljo <elaljooo@gmail.com>
2025-11-26 18:14:02 +0100

    agencies page done

--- app/agencies/page.tsx
+++ app/agencies/page.tsx
@@ -1,7 +1,36 @@
-export default function contacts(){
-    return(
-        <div>
-           agencies
-        </div>
-    )
+import { loadAgencies } from "@/lib/csvToJson"
+import styles from "./agencies.module.css"
+
+export default function agenciesPage(){
+    const agencies = loadAgencies();
+
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}> Agencies Details</h1>
+      <div className={styles.tableWrapper}>
+        <table className={styles.table}>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>State</th>
+              <th>Type</th>
+              <th>Population</th>
+              <th>Website</th>
+            </tr>
+          </thead>
+          <tbody>
+            {agencies.map((agency: any, index: number) => (
+              <tr key={index}>
+                <td>{index} {agency.name}</td>
+                <td>{agency.state}</td>
+                <td>{agency.type}</td>
+                <td>{agency.population}</td>
+                <td>{agency.website}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }

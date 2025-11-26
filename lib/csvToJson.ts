9b37ee6bb146e3d25d0e68925373e871f8775d2e
elaljo <elaljooo@gmail.com>
2025-11-26 18:13:21 +0100

    change papaparser to csv-parse tool

--- lib/csvToJson.ts
+++ lib/csvToJson.ts
@@ -1,17 +1,14 @@
-import Papa from "papaparse";
-import fs from "fs";
-import path from "path";
+import { parse } from "csv-parse/sync";
+import * as fs from "fs";
+import * as path from "path";
 
 export function loadAgencies() {
   const filePath = path.join(process.cwd(), "public", "agencies_agency_rows.csv");
   const csvData = fs.readFileSync(filePath, "utf-8");
 
-  const parsed = Papa.parse(csvData, {
-    header: true,      // convert each row into an object & uses the first line (City, Phone, Address…) as field names
+  const records  = parse(csvData, {
+    columns: true,      // uses the first line (City, Phone, Address…) as field names
     skipEmptyLines: true, // ignore empty rows in the CSV
   });
-  console.log(parsed.data);
-  return parsed.data; // this is now an array of agencies
+  return records;
 }
-
-loadAgencies();

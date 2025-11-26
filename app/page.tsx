24053817a2b5953b65935fc73d9551fc26a5a665
elaljo <elaljooo@gmail.com>
2025-11-26 15:06:04 +0100

    Home Page done

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,17 @@ import { currentUser } from "@clerk/nextjs/server";
 export default async function Home() {
   const user = await currentUser()
   const username = user?.username;
-  const welcomeSuffix = username ? `, ${username}` : '';
+  const welcomeSuffix = username ? ` ${username} 👋` : '';
 
   return (
-    <div>
-      <h1> Welcome{welcomeSuffix} 👋</h1>
-      <p> Welcome to our interactive dashboard!</p>
-      <p> Here, you can explore all our agencies and their associated employee contacts in one convenient place. Click on an agency to view its details and quickly access contact information for its employees. This tool makes it easy to navigate our network, stay connected, and find the information you need efficiently.</p>
+    <div className={styles.HomePage}>
+      <h1 className={styles.Welcome}> Welcome {welcomeSuffix}</h1>
+      <p className={styles.P1}> Welcome to our interactive dashboard!</p>
+      <div className={styles.details}>
+        <p className={styles.D1}> Here, you can explore all our agencies and their associated employee contacts in one convenient place.</p>
+        <p className={styles.D2}> 💡Click on an agency to view its details and quickly access contact information for its employees.</p>
+        <p className={styles.D3}> This tool makes it easy to navigate our network, stay connected, and find the information you need efficiently.</p>
+      </div>
     </div>
   );
 }

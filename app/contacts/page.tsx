33657a272e6a9b04e4410c8d657610b2f3f29da3
elaljo <elaljooo@gmail.com>
2025-11-27 23:41:10 +0100

    modified the page.tsx

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -1,40 +1,17 @@
 import { loadContacts } from "@/lib/csvToJson"
-import styles from "./contacts.module.css"
+import ContactsTable from "@/app/components/ContactsTable"
+
+interface Contact {
+  first_name: string;
+  last_name: string;
+  email: string;
+  phone: string;
+  title: string;
+  department: string;
+}
 
 export default async function contactspage(){
-    const contacts = await loadContacts();
+    const contacts = await loadContacts() as Contact[];
 
-  return (
-    <div className={styles.container}>
-      <h1 className={styles.title}> Contacts Details</h1>
-      <div className={styles.tableWrapper}>
-        <table className={styles.table}>
-          <thead>
-            <tr>
-                <th>No.</th>
-                <th>First Name</th>
-                <th>Last Name</th>
-                <th>Email</th>
-                <th>Phone</th>
-                <th>Title</th>
-                <th>Department</th>
-            </tr>
-          </thead>
-          <tbody>
-            {contacts.map((contact: any, index: number) => (
-              <tr key={index}>
-                <td>{index}</td>
-                <td>{contact.first_name}</td>
-                <td>{contact.last_name}</td>
-                <td>{contact.email}</td>
-                <td>{contact.phone}</td>
-                <td>{contact.title}</td>
-                <td>{contact.department}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </div>
-  );
+  return <ContactsTable contacts={contacts} />;
 }

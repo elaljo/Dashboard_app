df37ec9bc25cb258876c195b342bdb75f87542d5
elaljo <elaljooo@gmail.com>
2025-11-27 03:06:08 +0100

    home page done

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,34 @@
+import CardBox from "./components/CardBox";
 import styles from "./page.module.css";
 import { currentUser } from "@clerk/nextjs/server";
+
 export default async function Home() {
   const user = await currentUser()
   const username = user?.username;
-  const welcomeSuffix = username ? ` ${username} 👋` : '';
+  const welcomeSuffix = username ? ` ${username} ` : '';
 
   return (
     <div className={styles.HomePage}>
-      <h1 className={styles.Welcome}> Welcome {welcomeSuffix}</h1>
-      <p className={styles.P1}> Welcome to our interactive dashboard!</p>
-      <div className={styles.details}>
-        <p className={styles.D1}> Here, you can explore all our agencies and their associated employee contacts in one convenient place.</p>
-        <p className={styles.D2}> 💡Click on an agency to view its details and quickly access contact information for its employees.</p>
-        <p className={styles.D3}> This tool makes it easy to navigate our network, stay connected, and find the information you need efficiently.</p>
+      <h1> Welcome {welcomeSuffix} to our interactive dashboard!</h1>
+      <p className={styles.P1}> Here, you can explore all our agencies and their associated employee contacts in one convenient place.</p>
+      <div className={styles.Boxes}>
+        <CardBox
+          icon="/agency.svg"
+          title="Agencies"
+          description="Click on view details to explore all of our agencies"
+          buttonText="View Details"
+          href="/agencies"
+          />
+
+        <CardBox
+          icon="/contact.svg"
+          title="Contacts"
+          description="Click on view details to see all contacts that's working across our agencies"
+          buttonText="View Details"
+          href="/contacts"
+        />
       </div>
+      <p className={styles.D2}> This tool makes it easy to navigate our network, stay connected, and find the information you need efficiently.</p>
     </div>
   );
 }

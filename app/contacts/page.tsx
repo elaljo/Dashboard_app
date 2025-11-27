407bdb31c3f682be9f5eeb10639e237fcc5826b4
elaljo <elaljooo@gmail.com>
2025-11-27 16:34:28 +0100

    modified the contacts page

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -1,7 +1,40 @@
-export default function contacts(){
-    return(
-        <div>
-            contacts
-        </div>
-    )
+import { loadContacts } from "@/lib/csvToJson"
+import styles from "./contacts.module.css"
+
+export default function contactspage(){
+    const contacts = loadContacts();
+
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}> Contacts Details</h1>
+      <div className={styles.tableWrapper}>
+        <table className={styles.table}>
+          <thead>
+            <tr>
+                <th>No.</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Email</th>
+              <th>Phone</th>
+              <th>Title</th>
+              <th>Department</th>
+            </tr>
+          </thead>
+          <tbody>
+            {contacts.map((contact: any, index: number) => (
+              <tr key={index}>
+                <td>{index}</td>
+                <td>{contact.first_name}</td>
+                <td>{contact.last_name}</td>
+                <td>{contact.email}</td>
+                <td>{contact.phone}</td>
+                <td>{contact.title}</td>
+                <td>{contact.department}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }

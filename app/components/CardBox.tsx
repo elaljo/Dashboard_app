9d06d279581be3539cde5c9877f1e94ca26719f1
elaljo <elaljooo@gmail.com>
2025-11-27 03:07:36 +0100

    create CardBox Component

---
+++ app/components/CardBox.tsx
@@ -0,0 +1,26 @@
+import styles from "./CardBox.module.css"
+import Link from "next/link";
+
+interface CardBoxProps{
+    icon: string;
+    title: string;
+    description: string;
+    buttonText: string;
+    href: string;
+}
+
+export default function CardBox({ icon, title, description, buttonText, href}: CardBoxProps) {
+    return (
+        <div className={styles.Box}>
+            <div className={styles.agency}>
+                <img className={styles.svg} src={icon} alt={title} width={38} height={38} /> {title}
+            </div>
+            <div className={styles.boxDetails}>
+                {description}
+            </div>
+            <Link href={href} className={styles.BoxBtn}>
+                {buttonText}
+            </Link>
+        </div>
+    );
+}

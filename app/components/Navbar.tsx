5ac26334fb0f5efd74d2d2239306d4ddb84664f6
elaljo <elaljooo@gmail.com>
2025-11-28 11:51:36 +0100

    fix navbar in center

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -9,32 +9,34 @@ import {
 import Link from "next/link"
 import styles from "./Navbar.module.css"
 
-export default function Navbar(){
-    return(
-        <>
-            <Link href='/'> <div className={styles.AppName}>Dashboard</div> </Link>
-            <ul className={styles.ul}>
-                <Link href='/agencies'> <li className={styles.li}> Agencies </li> </Link>
-                <Link href='/contacts'> <li className={styles.li}> Contacts </li> </Link>
-            </ul>
-            <div className={styles.buttons}>
-                <SignedOut>
-                    <SignInButton>
-                        <button className={styles.signBtn}>
-                            SIGN IN
-                        </button>
-                    </SignInButton>
-                    <SignUpButton>
-                        <button className={styles.signBtn}>
-                            SIGN UP
-                        </button>
-                    </SignUpButton>
-                </SignedOut>
-                <SignedIn>
-                    <UserButton showName/>
-                </SignedIn>
-            </div>
-        </>
-
-    )
-}
+export default function Navbar() {
+    return (
+      <nav className={styles.container}>
+        <div className={styles.left}>
+          <Link href="/"><div className={styles.AppName}>Dashboard</div></Link>
+        </div>
+  
+        <div className={styles.center}>
+          <ul className={styles.ul}>
+            <Link href="/agencies"><li className={styles.li}>Agencies</li></Link>
+            <Link href="/contacts"><li className={styles.li}>Contacts</li></Link>
+          </ul>
+        </div>
+  
+        <div className={styles.right}>
+          <SignedOut>
+            <SignInButton>
+              <button className={styles.signBtn}>SIGN IN</button>
+            </SignInButton>
+            <SignUpButton>
+              <button className={styles.signBtn}>SIGN UP</button>
+            </SignUpButton>
+          </SignedOut>
+  
+          <SignedIn>
+            <UserButton showName />
+          </SignedIn>
+        </div>
+      </nav>
+    );
+  }

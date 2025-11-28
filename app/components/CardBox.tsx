a6b3aa9f5481a988f1fa240e744892ca6deb25bf
elaljo <elaljooo@gmail.com>
2025-11-28 11:39:49 +0100

    modified the description card box

--- app/components/CardBox.tsx
+++ app/components/CardBox.tsx
@@ -12,11 +12,13 @@ interface CardBoxProps{
 export default function CardBox({ icon, title, description, buttonText, href}: CardBoxProps) {
     return (
         <div className={styles.Box}>
-            <div className={styles.agency}>
-                <img className={styles.svg} src={icon} alt={title} width={50} height={50} /> {title}
-            </div>
-            <div className={styles.boxDetails}>
-                {description}
+            <div>
+                <div className={styles.agency}>
+                    <img className={styles.svg} src={icon} alt={title} width={50} height={50} /> {title}
+                </div>
+                <div className={styles.boxDetails}>
+                    {description}
+                </div>
             </div>
             <Link href={href} className={styles.BoxBtn}>
                 {buttonText}
